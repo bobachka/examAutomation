7e333aa074d5b5da19cd7ad25dd92d1fb97e8cc6
Sviatlana <sviatlana.balabanovich@gmail.com>
2020-07-09 23:50:54 +0300

    examAutomation search tests

--- src/test/java/exam/by/wildberries_project/functional/tests/WBSearchTests.java
+++ src/test/java/exam/by/wildberries_project/functional/tests/WBSearchTests.java
@@ -2,9 +2,11 @@ package exam.by.wildberries_project.functional.tests;
 
 import exam.by.wildberries_project.functional.entities.helpers.WBHomeHelper;
 import io.qameta.allure.Description;
+import io.qameta.allure.Flaky;
 import io.qameta.allure.Issue;
 import org.testng.Assert;
 import org.testng.annotations.AfterTest;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 public class WBSearchTests {
@@ -31,6 +33,51 @@ public class WBSearchTests {
         Assert.assertTrue(countPerPage > 0, "Nothing is found:");
     }
 
+    @Test
+    @Description(value = "Тест проверяет вывод результатов поиска на страницу")
+    public void countSearchResultsPerPage() {
+        int countPerPage = homeHelper.searchViaBtn(searchKey).getSearchResultsPerPage();
+        System.out.println("Results per page for " + searchKey + " are " + countPerPage);
+        Assert.assertTrue(countPerPage > 0, "Nothing is found:");
+    }
+
+    @Test
+    @Description(value = "Тест проверяет вывод общего количесва результатов")
+    public void countSearchResultsTotal() {
+        int countTotal = homeHelper.searchViaBtn(searchKey).getSearchResultsTotal();
+        System.out.println("Total Results for " + searchKey + " are " + countTotal);
+        Assert.assertTrue(countTotal > 0, "Nothing is found:");
+    }
+
+    @Test(dataProvider = "nonExistentKeys")
+    @Flaky
+    @Description(value = "Тест проверяет поиск невалидных значений, использует дата провайдер")
+    public void searchNonExistent(String searchKey) {
+        boolean resultsTitle = homeHelper.searchViaBtn(searchKey).isResultsTitleExist();
+        Assert.assertTrue(resultsTitle, "Something is found, but should be:");
+    }
+
+    @DataProvider(name = "nonExistentKeys")
+    public Object[] provideDate() {
+        return new Object[][]{{"jeans"}, {"белиберда"}, {"tututu"}};
+    }
+
+
+    @Test
+    @Description(value = "Тест проверяет поиск кириллицей")
+    public void searchCyrillic() {
+        String searchKey = "привет";
+        int countPerPage = homeHelper.searchViaEnter(searchKey).getSearchResultsPerPage();
+        Assert.assertTrue(countPerPage > 0, "Nothing is found:");
+    }
+
+    @Test
+    @Description(value = "Тест проверяет поиск спецсимволов и цифр")
+    public void searchSpecialChars() {
+        String searchKey = "!@#$%^&*()_+1234567890";
+        boolean resultsTitle = homeHelper.searchViaBtn(searchKey).isResultsTitleExist();
+        Assert.assertTrue(resultsTitle, "Something is found, but shouldn't be:");
+    }
 
     @Test
     @Issue(value = "COVID-19")
